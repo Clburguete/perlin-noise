2fea7a4b327b2d0a31905a71c8aa94a858e859a5
Carlos Luis <clburguete@gmail.com>
2017-08-28 19:40:24 +0200

    blur effect

--- particle.js
+++ particle.js
@@ -2,29 +2,63 @@
 
 function Particle() {
     this.pos = createVector(random(width),random(height));
-    this.vel = p5.Vector.random2D();
+    this.vel = createVector(0,0);
     this.acc = createVector(0,0);
+    this.maxSpeed = 4;
+
+    this.prevPos = this.pos.copy();
 
     this.update = function() {
       this.vel.add(this.acc);
+      this.vel.limit(this.maxSpeed);
       this.pos.add(this.vel);
       this.acc.mult(0);
     };
 
     this.applyForce = function(force) {
-
-      this.acc.ad(force);
+      this.acc.add(force);
     };
 
-    this.show = function() {
-      stroke(0);
-      point(this.pos.x, this.pos.y);
+    this.show = function(strokeColor,color1,color2,color3,transparency) {
+
+      stroke(color1,color2,color3,transparency);
+      strokeWeight(strokeColor);
+      line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+      this.updatePrev();
+      //point(this.pos.x, this.pos.y);
+    };
+
+    this.follow = function(vectors) {
+      var x = floor(this.pos.x / scl);
+      var y = floor(this.pos.y / scl);
+      var index = x + y * cols;
+      var force = vectors[index];
+      this.applyForce(force);
+      //CHECK VIDEO!!!
+    };
+
+    this.updatePrev = function() {
+      this.prevPos.x = this.pos.x;
+      this.prevPos.y = this.pos.y;
     };
 
     this.edges = function () {
-      if (this.pos.x > width) this.pos.x = 0;
-      if (this.pos.x < 0) this.pos.x = width;
-      if (this.pos.y > height) this.pos.y = 0;
-      if (this.pos.y < 0) this.pos.y = height;
+
+      if (this.pos.x > width) {
+        this.pos.x = 0;
+        this.updatePrev();
+      }
+      if (this.pos.x < 0){
+        this.pos.x = width;
+        this.updatePrev();
+      }
+      if (this.pos.y > height) {
+        this.pos.y = 0;
+        this.updatePrev();
+      }
+      if (this.pos.y < 0) {
+        this.pos.y = height;
+        this.updatePrev();
+      }
     };
 }
--- perlin_noise.js
+++ perlin_noise.js
@@ -9,55 +9,92 @@
 // a series of random waveforms
 // var xOff = 0;
 // var yOff= 100;
-var inc = 0.05;
+var inc = 0.5;
 var scl = 10;
 var cols, rows;
 
 var zOffset = 0;
+var mousePos;
 
 var fr;
 
 var particles = [];
 
+var flowField = [];
+var printAngle;
 function setup() {
-  createCanvas(400,400);
-  pixelDensity(1);
+  createCanvas(windowWidth,windowHeight);
+  pixelDensity(5);
   cols = floor(width / scl);
   rows = floor(height / scl);
-  fr = createP(' ');
 
-  for(var i = 0; i <100; i++){
+  noCursor();
+
+  flowField = new Array(cols*rows);
+  background(255);
+  for(var i = 0; i <200; i++){
     particles[i] = new Particle();
   }
+
 }
 
 function draw() {
-  background(255);
+  background(255,10);
   var yOffset = 0;
+  mousePos = mouseX+mouseY;
   for(var y = 0; y < rows; y++){
     var xOffset = 0;
     for(var x= 0; x < cols; x++){
-      var index = (x + y * width) *4;
-      var angle = noise(xOffset, yOffset, zOffset) * TWO_PI;
+      var index = (x + y * cols);
       var v = p5.Vector.fromAngle(angle);
-      xOffset += inc;
-      stroke(0);
-      push();
-      translate(x * scl, y * scl);
-      rotate(v.heading());
-    //  line(0,0, scl, 0);
-      pop();
+      v.setMag(3);
+      flowField[index] = v;
+      var angle = noise(mousePos, mousePos, mousePos) * TWO_PI;
+      xOffset +=inc;
+      // stroke(0,50);
+      // strokeWeight(1);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // line(0,0, scl, 0);
+      // pop();
+      printAngle = angle;
     }
     yOffset += inc;
 
-    zOffset += inc/100;
+    zOffset += inc/1000;
   }
   for (var i = 0; i < particles.length; i ++){
-    particles[i].show();
-    particles[i].update();
+    var z = Math.floor(i*random(1));
     particles[i].edges();
-
+    if (z%2 == 0) {
+      particles[i].show(100,0,0,0,1);
+    } else {
+      particles[i].show(100,255,0,0,1);
+    }
+    particles[i].update();
+    particles[i].follow(flowField);
+   //fr.html(flowField.toString());
+    //ft.html(floor(printAngle.toString()));
   }
 
-  fr.html(floor(frameRate()));
+
+
+  // for (var k = 0; k < mouseParticles.length; k++) {
+  //   mouseParticles[k].edges();
+  //   mouseParticles[k].show(255,255,255,255);
+  //   mouseParticles[k].update();
+  //   mouseParticles[k].follow(flowMouse);
+  //
+  // }
+
+
+}
+function mousePressed() {
+  for (var i = 0; i < particles.length; i ++){
+    particles[i].edges();
+    particles[i].show(15,mouseX,mouseY,mouseX,40);
+
+}
+  mousePos *= 2;
 }
